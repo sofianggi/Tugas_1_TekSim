d5555e52d9582895dff3c06ca2461e08baf3f3f5
sofianggi <127853891+sofianggi@users.noreply.github.com>
2023-03-20 22:43:25 +0700

    Add files via upload

---
+++ Tugas Teksim_Sofi Anggi Astuti_B2A020047.R
@@ -0,0 +1,21 @@
+Additive_RNG<-function(a,z0,c,m,n) {
+  xi<-matrix(NA,n,3)
+  colnames(xi)<-c("az(i-1)+c","Xi","Ui")
+  for (i in 1:n)
+  {
+    xi[i,1]<-(a*z0+c)
+    xi[i,2]<-xi[i,1]%%m
+    xi[i,3]<-xi[1,2]/m
+    z0<-xi[1,2]
+  }
+  hist(xi[,3])
+  View(xi)
+}
+
+Bernouli_2<-function(n,p) {
+  i<-n
+  p<-p
+  X<-runif(i)
+  Y<-(X<=p+0)
+  (tabel<-table(Y)/length(Y))
+}
